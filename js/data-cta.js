69a2e6b13e53ab326d3b28bb558eb9dcc4b4f783
Arnaud Masselin <web@adfinitas.fr>
2015-11-26 16:07:59 +0000

    Updated 'js/data-cta.js' via CloudCannon

--- js/data-cta.js
+++ js/data-cta.js
@@ -15,16 +15,18 @@ var _SEGMENTS = {
 };
 */
 // --> Config générale
-var     _IRAISER_CID    =   26,
-        _IRAISER_URL    =   'don.spa.asso.fr';
-
-// - Ajout des CTA avec action éclaté pour WoopraTrack
-var     _WOOPRA_INTERACTION     =   'cta_don-noel15';
-
-var _SEGMENTS = {
-    'OPTIN':    {'arrondiSup':0, 'type': 'fixe', 'values':[45,56,75]} ,
-    'ADHERENT': {'arrondiSup':5, 'type': 'addition', 'values':[0,5,10]} ,
-    'DONATEUR': {'arrondiSup':5, 'type': 'percent',  'values':[0,25,50]} ,
-    'MIDDLE':   {'arrondiSup':5, 'type': 'percent', 'values':[0,25,50]} ,
-    'GD':       {'arrondiSup':0, 'type': 'libre'}
-};
+var _dataCTA = {
+    _IRAISER_CID    :   26,
+    _IRAISER_URL    :   'don.spa.asso.fr',
+    
+    // - Ajout des CTA avec action éclaté pour WoopraTrack
+    _WOOPRA_INTERACTION     :   'cta_don-noel15',
+    
+    _SEGMENTS : {
+        'OPTIN':    {'arrondiSup':0, 'type': 'fixe', 'values':[45,56,75]} ,
+        'ADHERENT': {'arrondiSup':5, 'type': 'addition', 'values':[0,5,10]} ,
+        'DONATEUR': {'arrondiSup':5, 'type': 'percent',  'values':[0,25,50]} ,
+        'MIDDLE':   {'arrondiSup':5, 'type': 'percent', 'values':[0,25,50]} ,
+        'GD':       {'arrondiSup':0, 'type': 'libre'}
+    }
+}
