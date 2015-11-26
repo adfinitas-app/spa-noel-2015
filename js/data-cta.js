bf68e4d67a5505355ec60cde69407b732c2df650
Arnaud Masselin <web@adfinitas.fr>
2015-11-26 15:39:42 +0000

    Updated 'js/data-cta.js' via CloudCannon

--- js/data-cta.js
+++ js/data-cta.js
@@ -6,9 +6,9 @@ var     _IRAISER_CID    =   26,
 var     _WOOPRA_INTERACTION     =   'cta_don-noel15';
 
 var _SEGMENTS = {
-    'OPTIN':    {'arrondiSup':0, 'type': 'fixe', 'values':[45,75,95]} ,
-    'ADHERENT': {'arrondiSup':5, 'type': 'addition', 'values':[0,2,5]} ,
-    'DONATEUR': {'arrondiSup':5, 'type': 'addition',  'values':[0,5,10]} ,
-    'MIDDLE':   {'arrondiSup':5, 'type': 'addition', 'values':[0,25,100]} ,
+    'OPTIN':    {'arrondiSup':0, 'type': 'fixe', 'values':[45,75,95]},
+    'ADHERENT': {'arrondiSup':5, 'type': 'addition', 'values':[0,2,5]},
+    'DONATEUR': {'arrondiSup':5, 'type': 'addition',  'values':[0,5,10]},
+    'MIDDLE':   {'arrondiSup':5, 'type': 'addition', 'values':[0,25,100]},
     'GD':       {'arrondiSup':0, 'type': 'libre'}
 };
