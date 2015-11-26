a8c79e6e01e9b17f8fa77c37d08b1cc592ab68fc
Arnaud Masselin <web@adfinitas.fr>
2015-11-26 15:43:53 +0000

    Updated 'js/data-cta.js' via CloudCannon

--- js/data-cta.js
+++ js/data-cta.js
@@ -12,3 +12,19 @@ var _SEGMENTS = {
     'MIDDLE':   {'arrondiSup':5, 'type': 'addition', 'values':[0,25,100]},
     'GD':       {'arrondiSup':0, 'type': 'libre'}
 };
+/*
+// --> Config générale
+var     _IRAISER_CID    =   26,
+        _IRAISER_URL    =   'don.spa.asso.fr';
+
+// - Ajout des CTA avec action éclaté pour WoopraTrack
+var     _WOOPRA_INTERACTION     =   'cta_don-noel15';
+
+var _SEGMENTS = {
+    'OPTIN':    {'arrondiSup':0, 'type': 'fixe', 'values':[45,56,75]} ,
+    'ADHERENT': {'arrondiSup':5, 'type': 'addition', 'values':[0,5,10]} ,
+    'DONATEUR': {'arrondiSup':5, 'type': 'percent',  'values':[0,25,50]} ,
+    'MIDDLE':   {'arrondiSup':5, 'type': 'percent', 'values':[0,25,50]} ,
+    'GD':       {'arrondiSup':0, 'type': 'libre'}
+};
+*/
