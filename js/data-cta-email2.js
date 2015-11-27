9c9aad1ceaa52070482ff80bccb3726e9eebc53e
Arnaud Masselin <web@adfinitas.fr>
2015-11-27 09:11:52 +0000

    Updated 'js/data-cta-email2.js' via CloudCannon

--- js/data-cta-email2.js
+++ js/data-cta-email2.js
@@ -24,7 +24,7 @@ var dataCTA = {
     
     SEGMENTS : {
         'OPTIN':    {'arrondiSup':0, 'type': 'fixe', 'values':[45,56,75]} ,
-        'ADHERENT': {'arrondiSup':5, 'type': 'addition', 'values':[0,5,10]} ,
+        'ADHERENT': {'arrondiSup':5, 'type': 'percent', 'values':[0,5,10]} ,
         'DONATEUR': {'arrondiSup':5, 'type': 'percent',  'values':[0,25,50]} ,
         'MIDDLE':   {'arrondiSup':5, 'type': 'percent', 'values':[0,25,50]} ,
         'GD':       {'arrondiSup':0, 'type': 'libre'}
